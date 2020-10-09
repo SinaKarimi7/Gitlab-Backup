2a70028cf431ca21aa820d071bca8cb1152140fb
Cinna <sinakarimi76@gmail.com>
2020-10-09 18:09:30 +0330

    👌 Update code due to code review changes

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,1 +1,10 @@
-from . import cli
+import sys
+import signal
+
+def signal_handler(sig, frame):
+    # Signal handler to handle SIGINT, usually when Ctrl+C is pressed.. or if SIGINT is sent.
+    # Thanks to https://stackoverflow.com/a/1112350/92837
+    print('\nCaught interrupt(did you press Ctrl+C?), stopping gitlab-bk')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
--- setup.py
+++ setup.py
@@ -1,42 +1,41 @@
+#!/usr/bin/env python3
+
 """Setup script for gitlab-bk"""
 
 import os.path
-import setuptools
+from setuptools import setup, find_packages
+from cli.constants import VERSION
 
-# The directory containing this file
-HERE = os.path.abspath(os.path.dirname(__file__))
+with open('README.md') as f:
+    README = f.read()
 
-# The text of the README file
-with open(os.path.join(HERE, "README.md")) as fid:
-    README = fid.read()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-# This call to setup() does all the work
-setuptools.setup(
+setup(
     name="gitlab-bk",
-    version="0.0.7",
+    version=VERSION,
     license="MIT",
     description="Clone all projects in Gitlab account by group at once.",
     author="Cinna Karimi",
     author_email="sinakarimi76@gmail.com",
     url="https://github.com/SinaKarimi7/Gitlab-Backup",
-    download_url = 'https://github.com/SinaKarimi7/Gitlab-Backup/archive/v_01.tar.gz',
-    keywords = ['GITLAB', 'BACKUP', 'CLONE', 'SANCTION'],
-    install_requires=[
-        "alive_progress>=1.6.1"
-    ],
+    download_url = "https://github.com/SinaKarimi7/Gitlab-Backup/archive/v_01.tar.gz",
+    keywords = ["GITLAB", "BACKUP", "CLONE", "SANCTION"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: System :: Archiving :: Backup',
-        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Developers",
+        "Topic :: System :: Archiving :: Backup",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
     ],
+    install_requires=requirements,
     long_description=README,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
     entry_points = {
-        'console_scripts': ['gitlab-bk=cli.cli:main'],
+        "console_scripts": ["gitlab-bk=cli.cli:main"],
     }
 )
